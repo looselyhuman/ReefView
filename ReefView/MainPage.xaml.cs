8570d2e82849e41baaf93363c4bf4805aacf0687
ifield <ifield@PRION>
2016-07-03 14:43:22 -0700

    Add project files.

---
+++ ReefView/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Web.Http;
+using Windows.Storage;
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ReefView
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Background = new ImageBrush(new BitmapImage(new Uri(BaseUriHelper.GetBaseUri(this), "Images/icon.png")))
+        }
+
+        #region methods
+
+        public void ApplyFormImage()
+        {
+            var image = new BitmapImage();
+            int BytesToRead = 100;
+
+            var client = new HttpClient();
+            var tempFolder = ApplicationData.Current.TemporaryFolder;
+            var fileName = new Random().Next().ToString("{0}.png");
+
+            var img = ImageLoader.LoadImage(new Uri(""));
+            myImage.Source = image;
+
+        }
+
+        #endregion
+
+    }
+}
